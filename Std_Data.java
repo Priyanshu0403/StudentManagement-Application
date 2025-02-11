b6af14291db73b968ed8c0f8b66bde8e9ea498b5
Priyanshu Sarvaiyya <155910967+Priyanshu0403@users.noreply.github.com>
2025-02-11 23:13:50 +0530

    A model for storing Student data

---
+++ Std_Data.java
@@ -0,0 +1,41 @@
+package com.ps.model;
+
+public class Std_Data {
+	private int id;
+	private String name;
+	private String course;
+	private int fees;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCourse() {
+		return course;
+	}
+
+	public void setCourse(String course) {
+		this.course = course;
+	}
+
+	public int getFees() {
+		return fees;
+	}
+
+	public void setFees(int fees) {
+		this.fees = fees;
+	}
+
+}
